15935de4aeb869c3e5396c89a55e49c64db1bc79
Spencer Schack <scs1818@gmail.com>
2014-02-26 15:40:35 -0800

    added skeleton

---
+++ proj1/jpg.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "jpg.h"
+
+/*
+ * Analyze a JPG file that contains Exif data.
+ * If it is a JPG file, print out all relevant metadata and return 0.
+ * If it isn't a JPG file, return -1 and print nothing.
+ */
+int analyze_jpg(FILE *f) {
+    /* YOU WRITE THIS PART */
+    return -1;
+}
---
+++ proj1/png.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <zlib.h>
+#include "png.h"
+
+/*
+ * Analyze a PNG file.
+ * If it is a PNG file, print out all relevant metadata and return 0.
+ * If it isn't a PNG file, return -1 and print nothing.
+ */
+int analyze_png(FILE *f) {
+    /* YOU WRITE THIS PART */
+    return -1;
+}
