dc9b32180e559141d102cd03625b433e524c97d9
Spencer Schack <scs1818@gmail.com>
2014-02-26 16:21:42 -0800

    added chunk parser framework for pngs

--- proj1/png.c
+++ proj1/png.c
@@ -5,10 +5,13 @@
 #include "png.h"
 
 // Every PNG starts with these 8 bytes.
-const static char* HEADER = "\x89\x50\x4e\x47\x0d\x0a\x1a\x0a";
+const static char HEADER[8] = "\x89\x50\x4e\x47\x0d\x0a\x1a\x0a";
 
-// Ensures that the first 8 bytes of 'f' are equal to 'HEADER'.
-int valid_header(FILE * f) {
+/* 
+ * Ensures that the first 8 bytes of 'f' are equal to 'HEADER'.
+ * If the header is valid, returns 1, otherwise 0.
+ */
+int valid_header(FILE *f) {
 	char c;
 	for(int i = 0; i < 8; i++) {
 		c = fgetc(f);
@@ -20,11 +23,28 @@ int valid_header(FILE * f) {
 }
 
 /*
+ * Reads from 'f' and attempts to parse a chunk.
+ * Returns 1 if a chunk is parsed, 0 if it was the last chunk in the file, and
+ * -1 if the chunk was invalid.
+ */
+int parse_chunk(FILE *f) {
+
+}
+
+/*
  * Analyze a PNG file.
  * If it is a PNG file, print out all relevant metadata and return 0.
  * If it isn't a PNG file, return -1 and print nothing.
  */
 int analyze_png(FILE *f) {
-	if(!valid_header(f)) { return -1; }
+	if(valid_header(f)) {
+		int c;
+		while(c = parse_chunk(f)) {
+			if(c == -1) {
+				return -1;
+			}
+		}
+		return 0;
+	}
     return -1;
 }
